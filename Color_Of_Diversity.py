d887f406bc50064230a3fdc49245ac25e6242d03
habaekk <haback1493@gmail.com>
2023-03-12 21:11:01 +0900

    modify line's falling speed

--- Color_Of_Diversity.py
+++ Color_Of_Diversity.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2 as cv
-from time import sleep
 
 canvas = np.full((480, 640, 3), 255, dtype=np.uint8)
 i = 0
@@ -9,10 +8,10 @@ while True:
 
     canvas = np.full((480, 640, 3), 255, dtype=np.uint8)
 
-    sleep(1)
-
-    cv.line(canvas, (320, i), (320, i+5), color=(200, 200, 200), thickness=2)
-    i += 1
+    cv.line(canvas, (320, i), (320, i+10), color=(200, 200, 200), thickness=2)
+    i += 5
+    if i+10 == 480:
+        i = 0
 
     canvas_copy = canvas.copy()
     cv.imshow('Shape Drawing', canvas_copy)
